794152239a889e9516f5838bf363b92c554811f8
jaygandhi18 <121659709+jaygandhi18@users.noreply.github.com>
2024-04-26 18:09:00 +0530

    initial commit

---
+++ js.js
@@ -0,0 +1,12 @@
+function linkBudget() {
+    window.location.href = "budget.html";
+  }
+  function linkInvestment(){
+    window.location.href='investing.html';
+  }
+  function linkPredictor(){
+    window.location.href='https://devank21-stockindex-main-709qpo.streamlit.app/';
+  }
+  function linkPortfoilio(){
+    window.location.href='investment_portfolio.html'
+  }
